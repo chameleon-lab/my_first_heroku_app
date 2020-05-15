fd3307aa7224f8cf9101bc8507f41868428ee581
o.tymchuk <o.tymchuk@krok.edu.ua>
2020-05-15 14:51:30 +0300

    4 commit

--- my_first_heroku_app/settings.py
+++ my_first_heroku_app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'start',
     'cities',
     'trains',
+
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,7 @@ WSGI_APPLICATION = 'my_first_heroku_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -101,8 +103,15 @@ DATABASES = {
 import dj_database_url
 db = dj_database_url.config()
 DATABASES['default'].update(db)
+"""
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
